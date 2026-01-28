77c7131efbe02f08c4f0511a4676a50a96b1f07c
suborna33 <152749142+suborna33@users.noreply.github.com>
2026-01-28 22:21:56 +0600

    login with google

--- src/components/__firebase_____auth_step.js
+++ src/components/__firebase_____auth_step.js
@@ -2,6 +2,13 @@
 1. create a project in firebase console 
 2. in the project install firebase: npm install firebase
 3. bring firebase config into your project (form  firebase website)
+4. by using useAuth create the auth
+5. in the login page: create a provider new GoogleAuthProvider()
+6. use signInWithPopup and pass auth and provider
+
+
+
+
 
 
 
